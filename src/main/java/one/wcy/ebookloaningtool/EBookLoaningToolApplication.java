4282fd9b4867873211259df77a9c5a6314911e5c
wzwtt <wzwttol@outlook.com>
2025-03-06 14:02:22 +0000

    create project

---
+++ src/main/java/one/wcy/ebookloaningtool/EBookLoaningToolApplication.java
@@ -0,0 +1,13 @@
+package one.wcy.ebookloaningtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookLoaningToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBookLoaningToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/wcy/ebookloaningtool/EBookLoaningToolApplicationTests.java
@@ -0,0 +1,13 @@
+package one.wcy.ebookloaningtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookLoaningToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
